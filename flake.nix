f29870b12ce8d2e45143cf1bee4f618d8c6a3979
Ryan Hellwege <rhellwege6@gmail.com>
2023-09-03 07:02:23 +0000

    using nix flake

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: 
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in
+  {
+    # define development environment
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = [
+        pkgs.nodejs
+        pkgs.bun
+        pkgs.just
+      ];
+
+      shellHook = ''
+        nu -e "just --list"
+      '';
+    };
+  };
+}
