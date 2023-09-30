4fe079bb120110a69015b8399fae8da59d2baa46
rhellwege <rhellwege6@gmail.com>
2023-09-29 17:46:42 -0700

    fancier colors and padding on mobile

--- src/components/HamburgerNav.tsx
+++ src/components/HamburgerNav.tsx
@@ -14,35 +14,37 @@ import Hamburger from 'hamburger-react';
 const HamburgerNav = (props: Props) => { 
     const [isOpen, setOpen] = useState(false);
     const [menuClass, setMenuClass] = useState("invisible");
-    const [backgroundClass, setBackgroundClass] = useState("bg-white");
+    const [backgroundClass, setBackgroundClass] = useState("m-0");
 
     const handleToggle = (toggled: boolean) => {
         if (toggled) {
             setOpen(true);
             setMenuClass("visible");
+            setBackgroundClass("mb-[--vertical-height]");
         }
         else {
             setOpen(false);
             setMenuClass("invisible");
+            setBackgroundClass("m-0");
         }
     };
 
     const links = props.sections.map((s) => (
         <div className="w-full h-full">
             <a className="" href={`${s.title === "Home" ? "/" : s.title}`}>
-                <div className={`w-full text-[--primary-normal] focus:text-[--primary-focused] duration-150 text-center p-2 rounded-lg
+                <div className={`w-full ${(s.title === props.active) ? "text-[--primary-normal]" : "text-white"}  duration-150 text-center p-2 rounded-lg
                             ${(s.title === props.active) ? "bg-white" : "bg-[--black]"} border-[--secondary-normal] focus:border-[--secondary-focused] border-2 flex justify-between text-lg`}>
                     <p>{s.title}</p>
-                    <img src={s.img} alt=""></img>
+                    <img className="fill-white" src={s.img} alt=""></img>
                 </div>
             </a>
         </div>)
     ); 
 
     return (
-        <nav className={backgroundClass + " p-2 fixed visible md:invisible z-20 w-full h-full"}>
+        <nav className={backgroundClass + " p-6 fixed visible md:invisible z-20 w-full h-[--vertical-height]"}>
             <Hamburger rounded size={50} onToggle={handleToggle} />
-            <div className={menuClass + " table"}>
+            <div className={menuClass + " w-full mt-1"}>
                 {links}     
             </div>
         </nav>
